f25d06fa329f9f61c5d03c65d0fede9379294354
S.Angelin Sukirtha <113704359+AngelinSukirtha@users.noreply.github.com>
2024-05-08 17:48:48 +0530

    SQL for project

--- GroceryProject.sql
+++ GroceryProject.sql
@@ -4,11 +4,16 @@ use g_app_project;
 create table groceryappproject(
 userName varchar(50),
 password varchar(50),
-phoneNo varchar(10));
-insert into groceryappproject (username, password, phoneNo) values ('Angelin','123','9876543212');
+phoneNo varchar(10),
+mailId varchar(50),
+-- userName1 varchar(50),
+-- password1 varchar(50),
+-- phoneNo1 varchar(10),
+-- mailId1 varchar(50));
+-- insert into groceryappproject (username, password, phoneNo) values ('Angelin','123','9876543212');
 drop database g_app_project;
 select * from groceryappproject;
-alter table groceryappproject add price int;
-alter table groceryappproject add sgst int;
-alter table groceryappproject add cgst int;
-alter table groceryappproject add totalamount int;
+alter table groceryappproject add userName1 varchar(50);
+alter table groceryappproject add password1 varchar(50);
+alter table groceryappproject add phoneNo1 varchar(10);
+alter table groceryappproject add mailId1 varchar(50);
